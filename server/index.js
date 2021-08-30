abab1d8fa841a60d663c44b19ae98ffa6ef773af
mariaykim <jsmkim09@gmail.com>
2021-08-30 11:18:45 -0500

    current progress on express-server refactoring

--- server/index.js
+++ server/index.js
@@ -1,19 +1,8 @@
-// var express = require('express');
-// const app = express();
-const http = require('http');
-
-
-//headers to allows CORS requests
-const headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10
-};
+var express = require('express');
+const app = express();
 
 const port = 3000;
 
-// TODO: Fill with strings of your favorite quotes :)
 const quotes = [
   'Always go one step further',
   'Champions are made when nobody is looking',
@@ -29,53 +18,51 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+// const handleRequest = function(req, res) {
+//   console.log(`Endpoint: ${req.url} Method: ${req.method}`);
 
-const handleRequest = function(req, res) {
-  console.log(`Endpoint: ${req.url} Method: ${req.method}`);
+//   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
+//   if (req.url == '/') {
+//     console.log('redirecting');
+//     res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
+//     res.end();
+//   }
 
-  // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
-  if (req.url == '/') {
-    console.log('redirecting');
-    res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
-    res.end();
-  }
+//   // TODO: GET ONE
+//   if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
+//     console.log('Getting one quote');
+//     var quoteNum = getRandomInt(0, quotes.length);
 
-  // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
-    console.log('Getting one quote');
-    var quoteNum = getRandomInt(0, quotes.length);
+//     res.writeHead(200);
+//     res.end(quotes[quoteNum]);
 
-    res.writeHead(200);
-    res.end(quotes[quoteNum]);
+//   }
+//   // TODO: POST/CREATE
+//   else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
+//     console.log('Posting a new quote');
+//     quotes.push(req.data);
+//     res.writeHead(201);
+//     res.end();
+//   }
 
-  }
-  // TODO: POST/CREATE
-  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
-    console.log('Posting a new quote');
-    quotes.push(req.data);
-    res.writeHead(201);
-    res.end();
-  }
+// //CATCH ALL ROUTE
+//   else {
+//     res.writeHead(404,headers);
+//     res.end('Page not found');
 
-//CATCH ALL ROUTE
-  else {
-    res.writeHead(404,headers);
-    res.end('Page not found');
+//   }
+// }
 
-  }
-}
+app.get('/', (req, res) {
+  console.log('redirecting');
+  res.render('index') //redirect to quote
+});
 
-
-// app.get('/', (req, res) {
-//   console.log('redirecting');
-//   res.render('index') //redirect to quote
-// });
-
-// app.get('/quote/' || '/quote', (req, res) {
-//   console.log('Getting one quote');
-//   var quoteNum = getRandomInt(0, quotes.length);
-//   res.send(quotes[quoteNum]);
-// });
+app.get('/quote/?', (req, res) {
+  console.log('Getting one quote');
+  var quoteNum = getRandomInt(0, quotes.length);
+  res.send(quotes[quoteNum]);
+});
 
 // app.post('/quote/' || '/quote' , (req, res) {
 //   console.log('Posting a new quote');
