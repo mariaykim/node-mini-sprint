80a9e1585a62157e70f15f4e70952656fe6162f1
mariaykim <jsmkim09@gmail.com>
2021-08-30 18:38:23 -0500

    STEP 2 express-server completed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
-var express = require('express');
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
+const router = express.Router();
 
-app.use(express.static(path.join(__dirname, '/client')));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client')));
 
 const port = 3000;
 
@@ -21,67 +27,25 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-// const handleRequest = function(req, res) {
-//   console.log(`Endpoint: ${req.url} Method: ${req.method}`);
-
-//   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
-//   if (req.url == '/') {
-//     console.log('redirecting');
-//     res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
-//     res.end();
-//   }
-
-//   // TODO: GET ONE
-//   if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
-//     console.log('Getting one quote');
-//     var quoteNum = getRandomInt(0, quotes.length);
-
-//     res.writeHead(200);
-//     res.end(quotes[quoteNum]);
-
-//   }
-//   // TODO: POST/CREATE
-//   else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
-//     console.log('Posting a new quote');
-//     quotes.push(req.data);
-//     res.writeHead(201);
-//     res.end();
-//   }
-
-// //CATCH ALL ROUTE
-//   else {
-//     res.writeHead(404,headers);
-//     res.end('Page not found');
-
-//   }
-// }
-
-app.get('/', (req, res) {
+app.get('/', (req, res) => {
   console.log('redirecting');
   res.render('index') //redirect to quote
 });
 
-app.get('/quote/?', (req, res) {
+app.get('/quote', (req, res) => {
   console.log('Getting one quote');
   var quoteNum = getRandomInt(0, quotes.length);
   res.send(quotes[quoteNum]);
 });
 
-app.post('/quote/' || '/quote' , (req, res) {
+app.post('/quote' , (req, res) => {
+  console.log(req.body);
   console.log('Posting a new quote');
-  quotes.push(req.data);
+  quotes.push(req.body.quote);
+  console.log(quotes);
   res.send('quote posted');
 })
 
-
-
-
-// const server = http.createServer(handleRequest);
-// server.listen(port);
 app.listen(port, () => {
   console.log(`Listening express server on port ${port}!`)
 });
-
-
-// console.log('Server is running in the terminal!');
-// console.log(`Listening on http://localhost:${port}`);
